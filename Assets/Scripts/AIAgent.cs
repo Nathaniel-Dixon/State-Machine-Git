3449247e65619e62657d4a8493edd94d44ced446
Nathaniel-Dixon <110149202+Nathaniel-Dixon@users.noreply.github.com>
2022-08-04 18:01:14 +1000

    Added  Statemachine
    Added funictions to ai agent
    and added statmachine

--- Assets/Scripts/AIAgent.cs
+++ Assets/Scripts/AIAgent.cs
@@ -8,21 +8,54 @@ using UnityEngine;
 
 public class AIAgent : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private float speed;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _speed;
 
-
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform[] _waypoints; //declare
+    [SerializeField] private int _waypointindex = 0;
+    // Start is called before the first frame update\
+    //Array
+    //pros: they are fast and simple
+    //cons: they cannot be resized
+    public bool IsPlayerInRange()
     {
+        if (Vector2.Distance(transform.position,
+            _player.transform.position) < 5f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void ChasePlayer()
+    {
+        MoveToPoint(_player.transform.position);
+    }
 
+    public void Patrol()
+    {
+        Vector2 waypointPosition = _waypoints[_waypointindex].position;
+
+        MoveToPoint(waypointPosition);
+
+        if (Vector2.Distance(transform.position, waypointPosition) < 0.1f)
+        {
+            _waypointindex++;
+        }
+        //_waypointIndex = (_waypointIndex+1) % _waypoints.length;
+        if (_waypointindex >= _waypoints.Length) //4
+        {
+            _waypointindex = 0;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void MoveToPoint(Vector2 point)
     {
         // this.transform.position;
-        Vector2 directionToPlayer = player.transform.position - transform.position;
+        Vector2 directionToPlayer = point - (Vector2)transform.position;
 
         //if you want to know the direction from A to B
         // A -----> B
@@ -30,7 +63,7 @@ public class AIAgent : MonoBehaviour
         if (directionToPlayer.magnitude > 0.1f)
         {
             directionToPlayer.Normalize();
-            directionToPlayer *= speed * Time.deltaTime;
+            directionToPlayer *= _speed * Time.deltaTime;
             transform.position += (Vector3)directionToPlayer;
         }
     }
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AIAgent))]
+public class StateMachine : MonoBehaviour
+{
+    public enum State
+    {
+        // patrol routine
+        Patrol,
+        // searching
+        Aware,
+        //chase/combat 
+        Combat,
+        // 
+        Flee,
+        //Is alseep
+        Sleep,
+    }
+
+    //The Ai's current state 
+    [SerializeField] private State _state;
+
+    private AIAgent _aiAgent;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        // Grabs the first AiAgent (or what is in the <>)
+        // and puts it in the variable
+        _aiAgent = GetComponent<AIAgent>();
+
+        NextState();
+    }
+
+    private void NextState()
+    {
+        switch (_state)
+        {
+            case State.Patrol:
+                StartCoroutine(PatrolState());
+                break;
+            case State.Combat:
+                StartCoroutine(CombatState());
+                break;
+            default:
+                Debug.LogWarning("_state doesnt exist withing NextState Function, stopping statemachine");
+                break;
+
+        }
+        //if (_state == State.Patrol)
+       // {
+            //StartCoroutine(PatrolState());
+       // }
+    }
+
+    private IEnumerator PatrolState()
+    {
+        Debug.Log("Patrol: Enter");
+
+        while (_state == State.Patrol) //continues till statement is false, looping eg x < 10 so would loop 10 times if x increases by 1 each time
+        {
+            _aiAgent.Patrol();
+            if(_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Combat;
+            }
+            yield return null; //wait a single frame
+        }
+        Debug.Log("Patrol: Exit");
+        NextState();
+    }
+
+    private IEnumerator CombatState()
+    {
+        Debug.Log("Combat: Enter");
+
+        while (_state == State.Combat) //continues till statement is false, looping eg x < 10 so would loop 10 times if x increases by 1 each time
+        {
+            // our code here
+            _aiAgent.ChasePlayer();
+            if (!_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Patrol;
+            }
+            yield return null; //wait a single frame
+        }
+        Debug.Log("Combat: Exit");
+        NextState();
+
+    }
+}
